dac23cb9affee2a26d237248c2f6ec976c64989e
123xsz <79332917+123xsz@users.noreply.github.com>
2022-11-11 14:59:48 +0800

    Complete
    201910603-2022.11.11

---
+++ js/jsForMessage.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    const button = document.getElementById('submit');
+    button.addEventListener('click', function () {
+        alert('留言已发送，感谢到访！')
+    })
+})
